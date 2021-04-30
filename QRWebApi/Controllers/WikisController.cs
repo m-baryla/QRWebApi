fef477e7f0c1967be12c80d523b72bc6d0236926
m-baryla <m-baryla@outlook.com>
2021-04-30 12:06:33 +0200

    Subject: Implement TicketsHistories get data.
    Version: v.0.0.1
    
    Comment: Implement TicketsHistories get data.
    
    Tag: #Done
    
    The end...

--- QRWebApi/Controllers/WikisController.cs
+++ QRWebApi/Controllers/WikisController.cs
@@ -29,100 +29,99 @@ namespace QRWebApi.Controllers
             return await _context.Wikis.ToListAsync();
         }
 
-        ///https://localhost:44337/api/Wikis/GetWikiDetail/
 
-        // GET: api/GetWikiDetail/5
+        // GET: /api/Wikis/GetWikiDetail/
         [HttpGet("GetWikiDetail/")]
         public async Task<ActionResult<IEnumerable<WikiDetails>>> GetWikiDetail()
         {
             var query = (from w in _context.Wikis
-                join l in _context.DictLocations on w.IdLocation equals l.Id
-                join e in _context.DictEquipments on w.IdEquipment equals e.Id
-                select new WikiDetails
-                {
-                    LocationName = l.LocationName,
-                    EquipmentName = e.EquipmentName,
-                    Topic = w.Topic,
-                    Description = w.Description,
-                    Photo = w.Photo
-                }).ToListAsync();
+                                    join l in _context.DictLocations on w.IdLocation equals l.Id
+                                    join e in _context.DictEquipments on w.IdEquipment equals e.Id
+                                    select new WikiDetails
+                                    {
+                                        LocationName = l.LocationName,
+                                        EquipmentName = e.EquipmentName,
+                                        Topic = w.Topic,
+                                        Description = w.Description,
+                                        Photo = w.Photo
+                                    }).ToListAsync();
 
             return await query;
         }
 
-        // GET: api/Wikis/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Wiki>> GetWiki(int id)
-        {
-            var wiki = await _context.Wikis.FindAsync(id);
+        //// GET: api/Wikis/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<Wiki>> GetWiki(int id)
+        //{
+        //    var wiki = await _context.Wikis.FindAsync(id);
 
-            if (wiki == null)
-            {
-                return NotFound();
-            }
+        //    if (wiki == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return wiki;
-        }
+        //    return wiki;
+        //}
 
-        // PUT: api/Wikis/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutWiki(int id, Wiki wiki)
-        {
-            if (id != wiki.Id)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/Wikis/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
+        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutWiki(int id, Wiki wiki)
+        //{
+        //    if (id != wiki.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(wiki).State = EntityState.Modified;
+        //    _context.Entry(wiki).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!WikiExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!WikiExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/Wikis
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Wiki>> PostWiki(Wiki wiki)
-        {
-            _context.Wikis.Add(wiki);
-            await _context.SaveChangesAsync();
+        //// POST: api/Wikis
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
+        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //[HttpPost]
+        //public async Task<ActionResult<Wiki>> PostWiki(Wiki wiki)
+        //{
+        //    _context.Wikis.Add(wiki);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetWiki", new { id = wiki.Id }, wiki);
-        }
+        //    return CreatedAtAction("GetWiki", new { id = wiki.Id }, wiki);
+        //}
 
-        // DELETE: api/Wikis/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Wiki>> DeleteWiki(int id)
-        {
-            var wiki = await _context.Wikis.FindAsync(id);
-            if (wiki == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Wikis/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<Wiki>> DeleteWiki(int id)
+        //{
+        //    var wiki = await _context.Wikis.FindAsync(id);
+        //    if (wiki == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Wikis.Remove(wiki);
-            await _context.SaveChangesAsync();
+        //    _context.Wikis.Remove(wiki);
+        //    await _context.SaveChangesAsync();
 
-            return wiki;
-        }
+        //    return wiki;
+        //}
 
         private bool WikiExists(int id)
         {
